596553916ba84b6664fdb05f6185a2820849e8f1
ENGRMike <michael.s.bruins@gmail.com>
2018-06-25 21:24:49 -0400

    updated input into a form, new issues....

--- static/app.js
+++ static/app.js
@@ -3,8 +3,8 @@ function select_twitterData() {
     change_twitter_color();
     remove_finance_dash();
 
-    document.getElementById("twitter_options").innerHTML = '<div class="box-background box-padding"><label for="eventSelect">Select Twitter Data Visualization</label><select id="eventSelect" onchange="selectViz(this.value)">\
-    <option value="viz1">Visual 1</option><option value="viz2">Visual 2</option><option value="viz3">Visual 3</option></select></div>'
+    document.getElementById("twitter_options").innerHTML = '<div class="box-background box-padding"><form><label for="eventSelect">Select Twitter Data Visualization</label><select id="eventSelect" onchange="selectViz(this.value)">\
+    <option value="viz1" selected>Visual 1</option><option value="viz2">Visual 2</option><option value="viz3">Visual 3</option></select></form></div>'
 
 }
 
@@ -12,11 +12,27 @@ function select_financeData() {
     //select financial data and highlight the button blue
     change_finance_color();
     remove_twitter_dash();
-    document.getElementById("finance_options").innerHTML = '<div class="box-background box-padding"><label for="eventSelect">Select Event:</label><select id="eventSelect" onchange="selectEvent(this.value)">\
-    <option value="cancel">Trump Cancels North Korea Summit</option><option value="reon">Trump Reopens North Korea Summit</option><option value="summitday">Day of The North Korea Summit</option>\
-    </select></div><div class="box-background box-padding"><label for="sourceSelect">Select Dataset:</label><select id="dataSelect" onchange="selectData(this.value)">\
-    <option value="dataset1">DOW</option><option value="dataset2">S&P 500</option>\
-    </select></div>';
+    document.getElementById("finance_options").innerHTML = '<div class="box-background box-padding">\
+                                                                <form name="myForm" action="" onsubmit="return selectFinancePlot()">Select Event:\
+                                                                    <select id="event" name="event">\
+                                                                        <option value="cancel" selected>Trump Cancels North Korea Summit</option>\
+                                                                        <option value="reon">Trump Reopens North Korea Summit</option>\
+                                                                        <option value="summitday">Day of The North Korea Summit</option>\
+                                                                    </select>\
+                                                                    Select Dataset:\
+                                                                    <select id="dataset" name="dataset">\
+                                                                        <option value="dataset1">DOW</option>\
+                                                                        <option value="dataset2">S&P 500</option>\
+                                                                    </select>\
+                                                                    <input type="submit" value="submit">\
+                                                                </form>\
+                                                            </div>'
+    
+    // <div class="box-background box-padding"><label for="eventSelect">Select Event:</label><select id="eventSelect" onchange="selectEvent(this.value)">\
+    // <option value="cancel" selected>Trump Cancels North Korea Summit</option><option value="reon">Trump Reopens North Korea Summit</option><option value="summitday">Day of The North Korea Summit</option>\
+    // </select></div><div class="box-background box-padding"><label for="sourceSelect">Select Dataset:</label><select id="dataSelect" onchange="selectData(this.value)">\
+    // <option value="dataset1" selected>DOW</option><option value="dataset2">S&P 500</option>\
+    // </select></div>';
 }
 
 function change_twitter_color() {
@@ -53,7 +69,9 @@ function remove_plotSpace() {
 }
 
 // SELECT THE EVENT FOR THE FINANCE PLOTS
-function selectEvent(event) {
+function selectFinancePlot() {
+    var event = document.forms["myForm"]["event"].value;
+    var dataset = document.forms["myForm"]["dataset"].value;
     switch(event){
         case "cancel":
             //Add the date range for event one here
@@ -65,6 +83,16 @@ function selectEvent(event) {
             break;
         case "summitday":
             console.log("Day of the summit selected")
+    }
+    switch(dataset){
+        case "dataset1":
+            //Add the dataset for the DOW plot here
+            console.log("dataset 1 selected");
+            break;
+        case "dataset2":
+            //Add the dataset for the NASDAQ plot here
+            console.log("dataset 2 selected");
+            break;
     }
 }
 // SELECT THE DATASET FOR THE FINANCE PLOT
