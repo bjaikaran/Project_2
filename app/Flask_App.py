7f8a5a96d14388501e04cb4bb68fe7d95a420f40
Bobby Jaikaran <bobbyjaikaran@gwu.edu>
2018-06-29 19:44:58 -0400

    fixed for pep8 lint

--- app/Flask_App.py
+++ app/Flask_App.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine, func
 
 from flask import (Flask, render_template, request, redirect, jsonify)
 
-#create database
+# create database
 engine = create_engine("sqlite:///data/stocks_hashtags_V1.sqlite")
 
 Base = automap_base()
@@ -20,25 +20,48 @@ session = Session(engine)
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
+
 @app.route("/stocks")
 def stock_select():
-    results = session.query(stock_data.date, stock_data.symbol, stock_data.source, stock_data.event, stock_data.open, stock_data.high, stock_data.low, stock_data.close, stock_data.volume).all()
+    results = session.query(
+        stock_data.date,
+        stock_data.symbol,
+        stock_data.source,
+        stock_data.event,
+        stock_data.open,
+        stock_data.high,
+        stock_data.low,
+        stock_data.close,
+        stock_data.volume
+        ).all()
 
     return jsonify(results)
+
 
 @app.route("/tickers")
 def get_tickers():
     results = session.query(stock_data.symbol).limit(7).all()
     return jsonify(results)
 
+
 @app.route("/stocks/<select>")
 def get_stock(select):
     ticker = select
-    query = [stock_data.symbol,stock_data.date, stock_data.source, stock_data.event, stock_data.open, stock_data.high, stock_data.low, stock_data.close, stock_data.volume]
+    query = [
+        stock_data.symbol,
+        stock_data.date,
+        stock_data.source,
+        stock_data.event,
+        stock_data.open,
+        stock_data.high,
+        stock_data.low,
+        stock_data.close,
+        stock_data.volume]
     results = session.query(*query).filter(stock_data.symbol == ticker).all()
 
     try:
@@ -46,12 +69,17 @@ def get_stock(select):
     except:
         return 'not found'
 
+
 @app.route("/hashtags")
 def hashtag_select():
-    results = session.query(hashtag_data.event, hashtag_data.hashtag, hashtag_data.count).all()
-    
+    results = session.query(
+        hashtag_data.event,
+        hashtag_data.hashtag,
+        hashtag_data.count
+        ).all()
+
     return jsonify(results)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
