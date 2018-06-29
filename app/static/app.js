9a11e95fabb26c565a1c98c678987651d1d14971
ENGRMike <michael.s.bruins@gmail.com>
2018-06-29 14:51:36 -0400

    updated web page, adding stock switching options for box plot

--- app/static/app.js
+++ app/static/app.js
@@ -1,16 +1,22 @@
 function select_twitterData() {
     //select twitter data and highlight the button blue
     change_twitter_color();
+    remove_options();
     remove_plotSpace();
     create_chubby_chart();
+
+
 }
 
 function select_financeData() {
     //select financial data and highlight the button blue
     change_finance_color();
     remove_plotSpace();
-    create_finance_Plot1();
-    create_finance_Plot2();
+    //create_finance_Plot1();
+    //create_finance_Plot2();
+
+    document.getElementById("finance_options").innerHTML = "<div class='box-background box-padding'><label for='Select Stock:'</label><select id='dataselect' onchange='initialize_page(this.value)'></select></div>"
+
 }
 
 function change_twitter_color() {
@@ -38,74 +44,85 @@ function remove_plotSpace() {
     };
 }
 
-function create_finance_Plot1(data) {
-    var data = 12;//define dataset as dictionary with 3 options so plotly can select between them
-
-    var layout = {
-        title:'Trump Reschedules the Summit: Financial Data Trends',
-        xaxis: {
-            range: [startDate, endDate],
-            type: "date"
-        },
-        yaxis: {
-            autorange: true,
-            type: "linear"
+function remove_options() {
+    var finance_options =document.getElementById("finance_options");
+    if (finance_options.firstChild) {
+        while(finance_options.firstChild) {
+            finance_options.removeChild(finance_options.firstChild)
         }
-    };
-    Plotly.plot('plotspace1', data, layout, {
-        updatemenus: [{
-            y: 1,
-            yanchor: 'top',
-            buttons: [{
-                method: 'restyle',
-                args: ['visible', [true, false, false]],
-                label: 'Dow Jones Industrial Average'
-            }, {
-                method: 'restyle',
-                args: ['visible', [false, true, false]],
-                label: 'Standards and Poors'
-            }, {
-                method: 'restyle',
-                args: ['visible', [false, false, true]],
-                label: 'Euro Equities Exchange'
-            }]
-        }],
-    });
+    }
+
+}
+
+function create_finance_Plot1(data) {
+    //define dataset as dictionary with 3 options so plotly can select between them
+    Plotly.d3.json('/stocks', function(error, response) {
+        if (error) throw error;
+
+        var date = [];
+        var symbol = [];
+        var event = [];
+        var open =[];
+        var high =[];
+        var low =[];
+        var close =[];
+
+        for (var i = 0; i <=210; i++){
+            date.push(response.date[i]);
+            symbol.push(response.symbol[i]);
+            event.push(response.event[i]);
+            open.push(response.open[i]);
+            high.push(response.high[i]);
+            low.push(response.low[i]);
+            close.push(response.close[i]);
+        }
+
+        var BP_trace = [{
+            x: date,
+            close: close,
+
+        }];
+
+        var FDX_trace = [{
+            x: date,
+            close: close,
+
+        }];
+
+        var INTC_trace = [{
+            x: date,
+            close: close,
+
+        }];
+
+        var MSFT_trace = [{
+            x: date,
+            close: close,
+
+        }];
+
+        var ORCL_trace = [{
+            x: date,
+            close: close,
+
+        }];
+
+        var UPS_trace = [{
+            x: date,
+            close: close,
+
+        }];
+
+        var XOM_trace = [{
+            x: date,
+            close: close,
+
+        }];
+    })
 }
 
 function create_finance_Plot2(data) {
-    var data = 2;//define dataset as dictionary with 3 options so plotly can select between them
 
-    var layout = {
-        title:'Trump Reschedules the Summit: Financial Data Trends',
-        xaxis: {
-            range: [startDate, endDate],
-            type: "date"
-        },
-        yaxis: {
-            autorange: true,
-            type: "linear"
-        }
-    };
-    Plotly.plot('plotspace2', data, layout, {
-        updatemenus: [{
-            y: 1,
-            yanchor: 'top',
-            buttons: [{
-                method: 'restyle',
-                args: ['visible', [true, false, false]],
-                label: 'Dow Jones Industrial Average'
-            }, {
-                method: 'restyle',
-                args: ['visible', [false, true, false]],
-                label: 'Standards and Poors'
-            }, {
-                method: 'restyle',
-                args: ['visible', [false, false, true]],
-                label: 'Euro Equities Exchange'
-            }]
-        }],
-    });
 }
 
 function create_chubby_chart() {
